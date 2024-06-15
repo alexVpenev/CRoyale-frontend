1e1122fef3dfdf6dc095c55bfe8a64bdd28c3229
alexVpenev <alexVpenev+github@proton.me>
2024-06-15 21:04:40 +0200

    initialized project

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Footer = () => (
+  <footer className="bg-light p-3 text-center">
+    {/* <div className="logo" /> */}
+    <p>
+      Project by <a href="https://github.com/alexVpenev">alexVpenev</a>
+    </p>
+  </footer>
+);
+
+export default Footer;
